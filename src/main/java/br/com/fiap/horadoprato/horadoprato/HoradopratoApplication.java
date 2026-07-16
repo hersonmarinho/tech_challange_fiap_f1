dcb8023a837bf657d77e394d9d04c585d68f36fa
hersonmarinho <herson_nm@hotmail.com>
2026-07-15 22:43:01 -0300

    incluindo a estrutura inicial do projeto

---
+++ src/main/java/br/com/fiap/horadoprato/horadoprato/HoradopratoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.horadoprato.horadoprato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoradopratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoradopratoApplication.class, args);
+	}
+
+}
